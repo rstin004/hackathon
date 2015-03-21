7d143c7c120fbe6c74ccc9a158a4d02cdd3e7ce8
rstin004 <rstin004@odu.edu>
2015-03-21 13:58:10 -0400

    Initial Upload
    Basic structure and text file

---
+++ matching service.cpp
@@ -0,0 +1,30 @@
+/*
+ *
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class profile()
+{
+    string fname,lname;
+    int points;
+    string email;
+    char day_avail;
+    string time_avail;
+    int numclass;
+    string classes;
+};
+
+int main()
+{
+    ifstream inFile;
+    ofstream outFile;
+
+    return 0;
+}
