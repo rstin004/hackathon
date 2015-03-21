8ab2eb6dc76ab9a31f2d1e1351c87d0f6fa59962
frostburnz <mmalkal@gmail.com>
2015-03-21 14:52:51 -0400

    edit1

--- matching_service.cpp
+++ matching_service.cpp
@@ -1,1 +1,12 @@
 #include "matching_service.hh"
+
+Profile::Profile(string f, string l, int pts, string em, char d, string t, int nc, string cl){
+    fname=f;
+    lname=l;
+    points=pts;
+    email=em;
+    day_avail=d;
+    time_avail=t;
+    numClass =nc;
+    classes =cl;
+}
--- matching_service.hh
+++ matching_service.hh
@@ -13,17 +13,32 @@
 
 using namespace std;
 
-class profile
+class Profile
 {
 public:
+    ///contructors
+    Profile(string f=" ", string l=" ", int pts=0, string em=" ", char d='M', string t=" ", int nc=1, string cl=" ");
+    ///print
 //private:
     string fname,lname;
     int points;
     string email;
     char day_avail;
     string time_avail;
-    int numclass;
+    int numClass;
     string classes;
 };
+
+
+
+///make a match
+
+///create a schedule
+
+///create a history entry
+
+///give out points
+
+///create a leaderboard
 
 #endif // MATCHING_SERVICE_HH_INCLUDED
